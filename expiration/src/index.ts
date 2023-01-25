f13d53512e59521c2601f5ddef9a508a30db81d0
Brian Iverson <brian.iverson@tuebora.com>
2023-01-24 18:33:31 -0600

    Small changes for remaining services

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -2,6 +2,7 @@ import { stanWrapper } from './stan-wrapper';
 import { OrderCreatedListener } from './events/listeners/order-created-listener';
 
 const start = async () => {
+    console.log('Starting...');
     if (!process.env.STAN_CLIENT_ID) {
         throw new Error('STAN_CLIENT_ID must be defined');
     }
